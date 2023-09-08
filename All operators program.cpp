bfa864a47b2d479bd5a7739269d9e71039590cfd
Shumaila Fareed <143423029+Muhammaddanish3456@users.noreply.github.com>
2023-09-08 13:22:16 -0300

    Add files via upload

---
+++ All operators program.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 30;
+    int b = 10;
+    // Arithmetic Operators
+    cout << " Result of airthmatic operatorrs are " << endl;
+    cout << " Addition of a and b is " << a+b << endl;
+    cout << " Subtraction  of a and b is " << a-b << endl;
+    cout << " Multiplication of a and b is " << a*b << endl;
+    cout << " Division of a over b is " << a/b << endl;
+    cout << " Modulas of a over b is " << a%b << endl;
+    cout << " Increases the value of a variable by 1 using post increment " << a++ << endl;
+    cout << " Increases the value of a variable by 1 using pre increment " << ++a << endl;
+    cout << " Decreases the value of b variable by 1 using post decrement " << b-- << endl;
+    cout << " Decreases the value of b variable by 1 using pre decrement " << --b << endl;
+    cout << " **********************" << endl;
+    // Assignment Operator
+    cout << " Result of Assignment and compound assignment operators are " << endl;
+    int age = 20;
+    cout << " The age is " << age << endl;
+    // Compound assignment and additional assignment operators 
+    age += 10;
+    age -= 6;
+    age *= 3;
+    age /= 3;
+    age %= 3;
+    // Comparison operators
+    cout << " (a > b)" << endl;
+    cout << " (a < b)" << endl;
+    cout << " (a >= b)" << endl;
+    cout << " (a <= b)" << endl;
+    cout << " (a <= b)" << endl;
+    cout << " (a != b)" << endl;
+    cout << " (a == b)" << endl;
+    // Logical operators 
+    bool isStudent = true;
+    bool isBoy = true;
+    cout << "********************" << endl;
+    cout << "result of logical operators are " << endl;
+    cout << (isStudent && isBoy) << endl;
+    cout << (isStudent || isBoy) << endl;
+    cout << (isStudent) << endl;
+    return 0;
+}
+    
+     
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ Data types program.c
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "the size of int is " << sizeof(int)<< "bytes" << endl;
+    cout<< "the size of int is " << sizeof(float) << "bytes" << endl;
+    cout<< "the size of int is " << sizeof(bool) << "bytes" << endl;
+    cout<< "the size of int is " << sizeof(char) << "bytes" << endl;
+    cout<< "the size of int is " << sizeof(double) << "bytes" << endl;
+    
+    return 0;
+}
